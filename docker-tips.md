49091cb984806a883a171d4d71a087390733cfc6
Tom Filla <tfilla1@gmail.com>
2023-01-17 23:06:09 -0700

    initial commit - docker runs caddy as a reverse proxy for a node app

---
+++ docker-tips.md
@@ -0,0 +1,4 @@
+# Docker
+
+docker volume create [projectName]_caddy_data
+docker compose up
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+const path = require('path');
+
+app.use((req, res, next) => {
+  console.log('Time: ', Date.now());
+  next();
+});
+
+app.use('/request-type', (req, res, next) => {
+  console.log('Request type: ', req.method);
+  next();
+});
+
+app.get('/', (req, res) => {
+  // res.send('Successful response.');
+  console.log(__dirname)
+  res.sendFile(path.join(__dirname, 'index.html'))
+});
+
+app.listen(3000, () => console.log('Example app is listening on port 3000.'));
