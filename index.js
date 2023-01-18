64cd53576b13d9baa539e5de3cbaabf94adf5254
Tom Filla <tfilla1@gmail.com>
2023-01-17 23:13:57 -0700

    renaming src to site

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use('/request-type', (req, res, next) => {
 app.get('/', (req, res) => {
   // res.send('Successful response.');
   console.log(__dirname)
-  res.sendFile(path.join(__dirname, 'index.html'))
+  res.sendFile(path.join(__dirname, '/site/index.html'))
 });
 
 app.listen(3000, () => console.log('Example app is listening on port 3000.'));
